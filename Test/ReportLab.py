be7f9018739ec93374de62d8e8cfde7f673ab767
Juan Biondi <juanernestobiondi@gmail.com>
2017-08-24 10:21:55 +0200

    Update ReportLab.py

--- Test/ReportLab.py
+++ Test/ReportLab.py
@@ -1,13 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Import Modules
+"""
+import os
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.lib.units import inch
 
-import os
+
+"""
+Declare functions
+"""
 
 
 def GenerateBackground2(c):
     """
-    
+    Generate the PDF to test creation of it
     """
     c = canvas.Canvas("hello.pdf", pagesize=A4)
     for image in os.listdir(os.getcwd()):
@@ -21,10 +31,12 @@ def GenerateBackground2(c):
             c.rect(0.1*inch,0.1*inch,8.1*inch,11.4*inch,stroke=0, fill=1)
             c.setFillColorRGB(255,255,255)
             c.rect(0.25*inch,0.25*inch,7.8*inch,11.1*inch,stroke=0, fill=1)
-            c.drawImage(image, 7*inch,0.35*inch, width=1*inch,height=1*inch,mask=None) 
+            c.drawImage(image, 7*inch,0.35*inch, width=1*inch,height=1*inch,mask=None)
             c.showPage()
     c.save()
 
 
-
+"""
+EXECUTE THE CODE.
+"""
 GenerateBackground2(c)
