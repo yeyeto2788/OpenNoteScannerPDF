955c78bc078fcae37679c7f4d6b8d3b3fb741ee4
yeyeto2788 <juanernestobiondi@gmail.com>
2019-05-02 16:24:13 +0200

    Delete unnecessary print statements and fix string formatting

--- open_note_scanner/pdf_generator/generator.py
+++ open_note_scanner/pdf_generator/generator.py
@@ -66,7 +66,7 @@ class PDFGenerator:
                 qr_img.add_data(final_file_name)
                 qr_img.make(fit=True)
                 img = qr_img.make_image()
-                with open('%s.png'%final_file_name, 'wb') as qr_img_file:
+                with open('{}.png'.format(final_file_name), 'wb') as qr_img_file:
                     img.save(qr_img_file)
             # Return to the base directory.
             os.chdir(self.base_dir)
@@ -119,8 +119,6 @@ class PDFGenerator:
         pdf_canvas = canvas.Canvas(self.pdf_name, pagesize=size)
 
         lst_files = os.listdir(self.qr_directory)
-        print(lst_files)
-        print(utils.sort_alphanumeric_list(lst_files))
 
         for image_file in utils.sort_alphanumeric_list(lst_files):
 
